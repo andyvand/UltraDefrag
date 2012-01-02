baee56379785e9015fcea83bebc140b10adc7347
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-01-02 18:18:13 +0000

    cleanup

--- src/setvars_VM-WIN7_Stefan.cmd
+++ src/setvars_VM-WIN7_Stefan.cmd
@@ -5,9 +5,9 @@ echo Set Stefans' environment variables...
 if not defined ORIG_USERNAME set ORIG_USERNAME=%USERNAME%
 
 :: DDK for Windows Server 2003 SP1
-:: set WINDDKBASE=C:\WinDDK\3790.1830
+set WINDDKBASE=C:\WinDDK\3790.1830
 :: WDK for Windows Server 2008
-set WINDDKBASE=C:\WinDDK\6001.18002
+:: set WINDDKBASE=C:\WinDDK\6001.18002
 :: Windows Driver Kit 7_1_0
 :: set WINDDKBASE=C:\WinDDK\7600.16385.1
 set WINSDKBASE=
