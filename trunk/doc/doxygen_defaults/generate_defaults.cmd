a04d2e97f54d875d8b6264876bb451a546757b6b
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-01-22 21:37:37 +0000

    added support to generate PDF handbook through LaTeX

--- trunk/doc/doxygen_defaults/generate_defaults.cmd
+++ trunk/doc/doxygen_defaults/generate_defaults.cmd
@@ -17,14 +17,17 @@ doxygen -g default_Doxyfile
 echo Generating Default HTML files with Search...
 echo PROJECT_NAME=Default >>default_Doxyfile
 doxygen -w html default_header_search.html default_footer_search.html default_doxygen_search.css default_Doxyfile
+doxygen -w latex default_header_search.tex default_footer_search.tex default_doxygen_search.sty default_Doxyfile
 
 echo Generating Default HTML files...
 echo SEARCHENGINE=NO >>default_Doxyfile
 doxygen -w html default_header_regular.html default_footer_regular.html default_doxygen_regular.css default_Doxyfile
+doxygen -w latex default_header_regular.tex default_footer_regular.tex default_doxygen_regular.sty default_Doxyfile
 
 echo Generating Default HTML files with Treeview...
 echo GENERATE_TREEVIEW=YES >>default_Doxyfile
 doxygen -w html default_header_tree.html default_footer_tree.html default_doxygen_tree.css default_Doxyfile
+doxygen -w latex default_header_tree.tex default_footer_tree.tex default_doxygen_tree.sty default_Doxyfile
 
 echo.
 pause
