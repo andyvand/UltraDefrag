44fa7181da9c1e15ffe708d38823c2e8c3d4429f
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-03-19 13:38:01 +0000

    [BUG_FIX] few little bugs fixed

--- src/gui/main.c
+++ src/gui/main.c
@@ -40,7 +40,7 @@ HWND hMap = NULL;
 WGX_FONT wgxFont = {{0},0};
 RECT win_rc; /* coordinates of main window */
 RECT r_rc;   /* coordinates of restored window */
-double pix_per_dialog_unit = PIX_PER_DIALOG_UNIT_96DPI;
+double fScale = 1.0f;
 UINT TaskbarButtonCreatedMsg = 0;
 UINT TaskbarCreatedMsg = 0;
 
@@ -296,13 +296,14 @@ static void InitMainWindowCoordinates(void)
     int center_on_the_screen = 0;
     int screen_width, screen_height;
     int width, height;
-    RECT rc;
+    HDC hDC;
 
     if(scale_by_dpi){
-        rc.top = rc.left = 0;
-        rc.right = rc.bottom = 100;
-        if(MapDialogRect(hWindow,&rc))
-            pix_per_dialog_unit = (double)(rc.right - rc.left) / 100;
+        hDC = GetDC(NULL);
+        if(hDC){
+            fScale = (double)GetDeviceCaps(hDC,LOGPIXELSX) / 96.0f;
+            ReleaseDC(NULL,hDC);
+        }
     }
 
     if(r_rc.left == UNDEFINED_COORD)
@@ -357,6 +358,9 @@ void ResizeMainWindow(int force)
         WgxDbgPrint("GetClientRect failed in RepositionMainWindowControls()!\n");
         return;
     }
+    
+    if(list_height == 0)
+        list_height = DPI(VLIST_HEIGHT);
     
     /* correct invalid list heights */
     min_list_height = GetMinVolListHeight();
