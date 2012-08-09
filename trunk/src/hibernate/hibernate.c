bb1c3e95472c7a85650c2ef1e798df3c9f50e062
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-08-09 17:11:50 +0000

    [BUG_FIX] error messages provided by operating system forced to be displayed correctly in non-English Windows editions

--- trunk/src/hibernate/hibernate.c
+++ trunk/src/hibernate/hibernate.c
@@ -14,40 +14,74 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void show_help(void)
+static void show_help(void)
 {
     printf(
-        "Hibernate for Windows - command line tool for Windows hibernation.\n\n"
         "Usage: \n"
         "  hibernate now - hibernates PC\n"
         "  hibernate /?  - displays this help\n"
         );
 }
 
-void HandleError(char *msg)
+/**
+ * @brief Prints Unicode string.
+ * @note Only characters compatible
+ * with the current locale (OEM by default)
+ * will be printed correctly. Otherwise
+ * the '?' characters will be printed instead.
+ */
+static void print_unicode_string(wchar_t *string)
+{
+    HANDLE hStdOut;
+    DWORD written;
+    
+    if(string){
+        hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        WriteConsoleW(hStdOut,string,wcslen(string),&written,NULL);
+    }
+}
+
+static void HandleError(char *msg)
 {
     DWORD error;
-    LPVOID error_message;
+    wchar_t *error_message;
+    int length;
 
     error = GetLastError();
     /* format message and display it on the screen */
-    if(FormatMessage( 
+    if(FormatMessageW( 
         FORMAT_MESSAGE_ALLOCATE_BUFFER | 
         FORMAT_MESSAGE_FROM_SYSTEM | 
         FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL,
         error,
         MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-        (LPTSTR) &error_message,
+        (LPWSTR)(void *)&error_message,
         0,
-        NULL )){
-            printf(msg,(LPCTSTR)error_message);
+        NULL)){
+            printf("%s: ",msg);
+            /* get rid of trailing dot and new line */
+            length = wcslen(error_message);
+            if(length > 0){
+                if(error_message[length - 1] == '\n')
+                    error_message[length - 1] = 0;
+            }
+            if(length > 1){
+                if(error_message[length - 2] == '\r')
+                    error_message[length - 2] = 0;
+            }
+            if(length > 2){
+                if(error_message[length - 3] == '.')
+                    error_message[length - 3] = 0;
+            }
+            print_unicode_string(error_message);
+            printf("!\n");
             LocalFree(error_message);
     } else {
         if(error == ERROR_COMMITMENT_LIMIT)
-            printf(msg,"not enough memory");
+            printf("%s: not enough memory!\n",msg);
         else
-            printf(msg,"unknown reason");
+            printf("%s: unknown reason!\n",msg);
     }
 }
 
@@ -55,8 +89,9 @@ int __cdecl main(int argc, char **argv)
 {
     HANDLE hToken; 
     TOKEN_PRIVILEGES tkp; 
-    int i;
-    int now = 0;
+    int i, now = 0;
+
+    printf("Hibernate for Windows - command line tool for Windows hibernation.\n\n");
 
     for(i = 0; i < argc; i++){
         if(!strcmp(argv[i],"now")) now = 1;
@@ -69,7 +104,7 @@ int __cdecl main(int argc, char **argv)
         /* enable shutdown privilege */
         if(!OpenProcessToken(GetCurrentProcess(), 
         TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,&hToken)){
-            HandleError("Cannot open process token: %s!\n");
+            HandleError("Cannot open process token");
             return 1;
         }
         
@@ -78,12 +113,13 @@ int __cdecl main(int argc, char **argv)
         tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
         AdjustTokenPrivileges(hToken,FALSE,&tkp,0,(PTOKEN_PRIVILEGES)NULL,0);         
         if(GetLastError() != ERROR_SUCCESS){
-            HandleError("Cannot set shutdown privilege: %s!\n");
+            HandleError("Cannot set shutdown privilege");
             return 1;
         }
         
-        if(!SetSuspendState(TRUE,FALSE,FALSE)){ /* hibernate, request permission from apps and drivers */
-            HandleError("Cannot hibernate PC: %s!\n");
+        /* hibernate, request permission from apps and drivers */
+        if(!SetSuspendState(TRUE,FALSE,FALSE)){
+            HandleError("Cannot hibernate the computer");
             return 1;
         }
     }
