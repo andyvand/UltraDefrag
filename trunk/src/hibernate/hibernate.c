c0dc0aed4f1e132f71ea9857bfed1081dc118ed6
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-08-13 11:56:32 +0000

    [BUG_FIX] error messages redirected to stderr

--- trunk/src/hibernate/hibernate.c
+++ trunk/src/hibernate/hibernate.c
@@ -1,12 +1,21 @@
 /*
-* Hibernate for Windows - a command line tool for Windows hibernation.
-*
-* Programmer:    Dmitri Arkhangelski (dmitriar@gmail.com)
-* Creation date: October 2009
-* License:       Public Domain
-*/
-
-/* Revised by Dmitri Arkhangelski, 2011, 2012 */
+ *  Hibernate for Windows - a command line tool for Windows hibernation.
+ *  Copyright (c) 2009-2012 Dmitri Arkhangelski (dmitriar@gmail.com).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <windows.h>
 #include <powrprof.h>
@@ -14,34 +23,38 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+#include "../dll/wgx/wgx.h"
+
 static void show_help(void)
 {
     printf(
+        "===============================================================================\n"
+        "Hibernate for Windows - a command line tool for Windows hibernation.\n"
+        "Copyright (c) UltraDefrag Development Team, 2009-2012.\n"
+        "\n"
+        "===============================================================================\n"
+        "This program is free software; you can redistribute it and/or\n"
+        "modify it under the terms of the GNU General Public License\n"
+        "as published by the Free Software Foundation; either version 2\n"
+        "of the License, or (at your option) any later version.\n"
+        "\n"
+        "This program is distributed in the hope that it will be useful,\n"
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+        "GNU General Public License for more details.\n"
+        "\n"
+        "You should have received a copy of the GNU General Public License\n"
+        "along with this program; if not, write to the Free Software\n"
+        "Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.\n"
+        "===============================================================================\n"
+        "\n"
         "Usage: \n"
         "  hibernate now - hibernates PC\n"
         "  hibernate /?  - displays this help\n"
         );
 }
 
-/**
- * @brief Prints Unicode string.
- * @note Only characters compatible
- * with the current locale (OEM by default)
- * will be printed correctly. Otherwise
- * the '?' characters will be printed instead.
- */
-static void print_unicode_string(wchar_t *string)
-{
-    HANDLE hStdOut;
-    DWORD written;
-    
-    if(string){
-        hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-        WriteConsoleW(hStdOut,string,wcslen(string),&written,NULL);
-    }
-}
-
-static void HandleError(char *msg)
+static void handle_error(char *msg)
 {
     DWORD error;
     wchar_t *error_message;
@@ -59,7 +72,7 @@ static void HandleError(char *msg)
         (LPWSTR)(void *)&error_message,
         0,
         NULL)){
-            printf("%s: ",msg);
+            fprintf(stderr,"%s: ",msg);
             /* get rid of trailing dot and new line */
             length = wcslen(error_message);
             if(length > 0){
@@ -74,24 +87,22 @@ static void HandleError(char *msg)
                 if(error_message[length - 3] == '.')
                     error_message[length - 3] = 0;
             }
-            print_unicode_string(error_message);
-            printf("!\n");
+            WgxPrintUnicodeString(error_message,stderr);
+            fprintf(stderr,"!\n");
             LocalFree(error_message);
     } else {
         if(error == ERROR_COMMITMENT_LIMIT)
-            printf("%s: not enough memory!\n",msg);
+            fprintf(stderr,"%s: not enough memory!\n",msg);
         else
-            printf("%s: unknown reason!\n",msg);
+            fprintf(stderr,"%s: unknown reason!\n",msg);
     }
 }
 
 int __cdecl main(int argc, char **argv)
 {
-    HANDLE hToken; 
-    TOKEN_PRIVILEGES tkp; 
     int i, now = 0;
-
-    printf("Hibernate for Windows - command line tool for Windows hibernation.\n\n");
+    HANDLE hToken; 
+    TOKEN_PRIVILEGES tkp;
 
     for(i = 0; i < argc; i++){
         if(!strcmp(argv[i],"now")) now = 1;
@@ -100,28 +111,32 @@ int __cdecl main(int argc, char **argv)
 
     if(now == 0){
         show_help();
-    } else {
-        /* enable shutdown privilege */
-        if(!OpenProcessToken(GetCurrentProcess(), 
-        TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,&hToken)){
-            HandleError("Cannot open process token");
-            return 1;
-        }
-        
-        LookupPrivilegeValue(NULL,SE_SHUTDOWN_NAME,&tkp.Privileges[0].Luid);
-        tkp.PrivilegeCount = 1;  // one privilege to set    
-        tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
-        AdjustTokenPrivileges(hToken,FALSE,&tkp,0,(PTOKEN_PRIVILEGES)NULL,0);         
-        if(GetLastError() != ERROR_SUCCESS){
-            HandleError("Cannot set shutdown privilege");
-            return 1;
-        }
-        
-        /* hibernate, request permission from apps and drivers */
-        if(!SetSuspendState(TRUE,FALSE,FALSE)){
-            HandleError("Cannot hibernate the computer");
-            return 1;
-        }
+        return 0;
+    }
+
+    printf("Hibernate for Windows - a command line tool for Windows hibernation.\n");
+    printf("Copyright (c) UltraDefrag Development Team, 2009-2012.\n\n");
+
+    /* enable shutdown privilege */
+    if(!OpenProcessToken(GetCurrentProcess(), 
+    TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,&hToken)){
+        handle_error("Cannot open process token");
+        return 1;
+    }
+    
+    LookupPrivilegeValue(NULL,SE_SHUTDOWN_NAME,&tkp.Privileges[0].Luid);
+    tkp.PrivilegeCount = 1;  // one privilege to set    
+    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
+    AdjustTokenPrivileges(hToken,FALSE,&tkp,0,(PTOKEN_PRIVILEGES)NULL,0);         
+    if(GetLastError() != ERROR_SUCCESS){
+        handle_error("Cannot set shutdown privilege");
+        return 1;
+    }
+    
+    /* hibernate, request permission from apps and drivers */
+    if(!SetSuspendState(TRUE,FALSE,FALSE)){
+        handle_error("Cannot hibernate the computer");
+        return 1;
     }
     return 0;
 }
