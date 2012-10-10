13c1d70a53ee6f9efc138a8a0a74df63439383c4
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-10-10 18:59:59 +0000

    cleanup

--- trunk/src/scripts/udreportcnv.lua
+++ trunk/src/scripts/udreportcnv.lua
@@ -252,7 +252,7 @@ header = [[
     <table class="links_toolbar" width="100%"><tbody>
       <tr>
         <td class="left"><a href="http://ultradefrag.sourceforge.net">$VISIT_HOMEPAGE</a></td>
-        <td class="center"><a href="file:///$instdir\options\udreportopts.lua">$VIEW_REPORT_OPTIONS</a></td>
+        <td class="center"><a href="file:///$instdir_utf8\options\udreportopts.lua">$VIEW_REPORT_OPTIONS</a></td>
         <td class="right"><a href="http://www.lua.org/">$POWERED_BY_LUA</a></td>
       </tr>
     </tbody></table>
@@ -273,7 +273,7 @@ footer = [[
     <table class="links_toolbar" width="100%"><tbody>
       <tr>
         <td class="left"><a href="http://ultradefrag.sourceforge.net">$VISIT_HOMEPAGE</a></td>
-        <td class="center"><a href="file:///$instdir\options\udreportopts.lua">$VIEW_REPORT_OPTIONS</a></td>
+        <td class="center"><a href="file:///$instdir_utf8\options\udreportopts.lua">$VIEW_REPORT_OPTIONS</a></td>
         <td class="right"><a href="http://www.lua.org/">$POWERED_BY_LUA</a></td>
       </tr>
     </tbody></table>
@@ -380,6 +380,9 @@ end
 -------------------------------------------------------------------------------
 report_path = arg[1]
 instdir     = arg[2]
+
+instdir_utf8 = os.getenv("UD_INSTALL_DIR")
+if not instdir_utf8 then instdir_utf8 = "" end
 
 assert(report_path, usage)
 assert(instdir, usage)
