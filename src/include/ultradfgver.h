aae9bc6c62bea75ed6912f59847391efdd0dc26b
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-12-24 13:42:42 +0000

    [RELEASE] UltraDefrag v6.0.0 beta2 released

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,5 +1,5 @@
 #define VERSION 6,0,0,0 
 #define VERSION2 "6, 0, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 6.0.0 beta1" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.0 beta1 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 6.0.0 beta1" 
+#define VERSIONINTITLE "UltraDefrag 6.0.0 beta2" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.0 beta2 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 6.0.0 beta2" 
