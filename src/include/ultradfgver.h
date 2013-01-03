19d3550446ab0ce246121e827ce0a53e6a276cf9
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-03 13:08:05 +0000

    added version string for about box

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -3,3 +3,4 @@
 #define VERSIONINTITLE "UltraDefrag 7.0.0 beta1" 
 #define VERSIONINTITLE_PORTABLE "UltraDefrag 7.0.0 beta1 Portable" 
 #define ABOUT_VERSION "Ultra Defragmenter version 7.0.0 beta1" 
+#define wxUD_ABOUT_VERSION "7.0.0 beta1" 
