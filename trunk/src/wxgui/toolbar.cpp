d166efbce66368d6233d014b7f161da9f3725687
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-04 10:48:31 +0000

    cleanup

--- trunk/src/wxgui/toolbar.cpp
+++ trunk/src/wxgui/toolbar.cpp
@@ -47,7 +47,7 @@
  */
 void MainFrame::InitToolbar()
 {
-    wxToolBar *toolBar = CreateToolBar(wxNO_BORDER | wxTB_HORIZONTAL | wxTB_FLAT);
+    m_ToolBar = CreateToolBar(wxNO_BORDER | wxTB_HORIZONTAL | wxTB_FLAT);
 
     wxBitmap toolbarImage = wxBITMAP(toolbar16);
     wxMask *imageMask = new wxMask(toolbarImage,wxColor(255,0,255,wxALPHA_OPAQUE));
@@ -61,27 +61,27 @@ void MainFrame::InitToolbar()
     for(i=0; i < TOOLBAR_IMAGE_COUNT; i++)
         toolBarBitmaps[i] = toolbarImage.GetSubBitmap(wxRect(bmHeight*i,0,bmHeight,bmHeight));
 
-    toolBar->SetToolBitmapSize(wxSize(bmHeight,bmHeight));
+    m_ToolBar->SetToolBitmapSize(wxSize(bmHeight,bmHeight));
 
-    toolBar->AddTool(ID_Analyze,   wxT("Analyze"),   toolBarBitmaps[0], _("Analyze"),                  wxITEM_NORMAL);
-    toolBar->AddTool(ID_Repeat,    wxT("Repeat"),    toolBarBitmaps[1], _("Repeat"),                   wxITEM_CHECK);
-    toolBar->AddTool(ID_Defrag,    wxT("Defrag"),    toolBarBitmaps[3], _("Defragment"),               wxITEM_NORMAL);
-    toolBar->AddTool(ID_QuickOpt,  wxT("QuickOpt"),  toolBarBitmaps[4], _("Quick Optimize"),           wxITEM_NORMAL);
-    toolBar->AddTool(ID_FullOpt,   wxT("FullOpt"),   toolBarBitmaps[5], _("Full Optimize"),            wxITEM_NORMAL);
-    toolBar->AddTool(ID_MftOpt,    wxT("MftOpt"),    toolBarBitmaps[6], _("Optimize MFT"),             wxITEM_NORMAL);
-    toolBar->AddTool(ID_Pause,     wxT("Pause"),     toolBarBitmaps[7], _("Pause"),                    wxITEM_NORMAL);
-    toolBar->AddTool(ID_Stop,      wxT("Stop"),      toolBarBitmaps[8], _("Stop"),                     wxITEM_NORMAL);
-    toolBar->AddSeparator();
-    toolBar->AddTool(ID_ShowReport,wxT("ShowReport"),toolBarBitmaps[9], _("Show Fragmentation Report"),wxITEM_NORMAL);
-    toolBar->AddSeparator();
-    toolBar->AddTool(ID_GuiOptions,wxT("GuiOptions"),toolBarBitmaps[10],_("Edit GUI Settings"),        wxITEM_NORMAL);
-    toolBar->AddSeparator();
-    toolBar->AddTool(ID_BootEnable,wxT("BootEnable"),toolBarBitmaps[11],_("Enable Boot"),              wxITEM_NORMAL);
-    toolBar->AddTool(ID_BootScript,wxT("BootScript"),toolBarBitmaps[12],_("Edit Boot Script"),         wxITEM_NORMAL);
-    toolBar->AddSeparator();
-    toolBar->AddTool(ID_HelpAbout, wxT("HelpAbout"), toolBarBitmaps[13],_("About"),                    wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_Analyze,   wxT("Analyze"),   toolBarBitmaps[0], _("Analyze"),                  wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_Repeat,    wxT("Repeat"),    toolBarBitmaps[1], _("Repeat"),                   wxITEM_CHECK);
+    m_ToolBar->AddTool(ID_Defrag,    wxT("Defrag"),    toolBarBitmaps[3], _("Defragment"),               wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_QuickOpt,  wxT("QuickOpt"),  toolBarBitmaps[4], _("Quick Optimize"),           wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_FullOpt,   wxT("FullOpt"),   toolBarBitmaps[5], _("Full Optimize"),            wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_MftOpt,    wxT("MftOpt"),    toolBarBitmaps[6], _("Optimize MFT"),             wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_Pause,     wxT("Pause"),     toolBarBitmaps[7], _("Pause"),                    wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_Stop,      wxT("Stop"),      toolBarBitmaps[8], _("Stop"),                     wxITEM_NORMAL);
+    m_ToolBar->AddSeparator();
+    m_ToolBar->AddTool(ID_ShowReport,wxT("ShowReport"),toolBarBitmaps[9], _("Show Fragmentation Report"),wxITEM_NORMAL);
+    m_ToolBar->AddSeparator();
+    m_ToolBar->AddTool(ID_GuiOptions,wxT("GuiOptions"),toolBarBitmaps[10],_("Edit GUI Settings"),        wxITEM_NORMAL);
+    m_ToolBar->AddSeparator();
+    m_ToolBar->AddTool(ID_BootEnable,wxT("BootEnable"),toolBarBitmaps[11],_("Enable Boot"),              wxITEM_NORMAL);
+    m_ToolBar->AddTool(ID_BootScript,wxT("BootScript"),toolBarBitmaps[12],_("Edit Boot Script"),         wxITEM_NORMAL);
+    m_ToolBar->AddSeparator();
+    m_ToolBar->AddTool(ID_HelpAbout, wxT("HelpAbout"), toolBarBitmaps[13],_("About"),                    wxITEM_NORMAL);
 
-    toolBar->Realize();
+    m_ToolBar->Realize();
 }
 
 /** @} */
