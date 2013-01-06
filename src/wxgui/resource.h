8714c0c16bb497dab1f71dfb19a82292e661d265
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-06 19:43:20 +0000

    sources prepared to use tool bar images for menu items

---
+++ src/wxgui/resource.h
@@ -0,0 +1,65 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  UltraDefrag - a powerful defragmentation tool for Windows NT.
+//  Copyright (c) 2007-2013 Dmitri Arkhangelski (dmitriar@gmail.com).
+//  Copyright (c) 2010-2013 Stefan Pendl (stefanpe@users.sourceforge.net).
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef _UDEFRAG_GUI_RESOURCE_H_
+#define _UDEFRAG_GUI_RESOURCE_H_
+
+// =======================================================================
+//                               Headers
+// =======================================================================
+
+// =======================================================================
+//                          Macro definitions
+// =======================================================================
+
+// =======================================================================
+//                            Declarations
+// =======================================================================
+
+// =======================================================================
+//                              Constants
+// =======================================================================
+
+enum {
+    // tool bar image identifiers
+    ID_BMP_Analyze,
+    ID_BMP_Repeat,
+    ID_BMP_Repeat_disabled,
+    ID_BMP_Defrag,
+    ID_BMP_QuickOpt,
+    ID_BMP_FullOpt,
+    ID_BMP_MftOpt,
+    ID_BMP_Pause,
+    ID_BMP_Stop,
+    ID_BMP_ShowReport,
+    ID_BMP_GuiOptions,
+    ID_BMP_BootEnable,
+    ID_BMP_BootScript,
+    ID_BMP_HelpContents,
+    TOOLBAR_IMAGE_COUNT // must always be last
+};
+
+// =======================================================================
+//                           Global variables
+// =======================================================================
+
+#endif /* _UDEFRAG_GUI_RESOURCE_H_ */
