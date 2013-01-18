fb87137854ede0c9d1af111d8b6533b208b26049
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-18 19:56:20 +0000

    empty list and map controls added

---
+++ trunk/src/wxgui/map.cpp
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  UltraDefrag - a powerful defragmentation tool for Windows NT.
+//  Copyright (c) 2007-2013 Dmitri Arkhangelski (dmitriar@gmail.com).
+//  Copyright (c) 2010-2013 Stefan Pendl (stefanpe@users.sourceforge.net).
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file map.cpp
+ * @brief Cluster map.
+ * @addtogroup ClusterMap
+ * @{
+ */
+
+// Ideas by Stefan Pendl <stefanpe@users.sourceforge.net>
+// and Dmitri Arkhangelski <dmitriar@gmail.com>.
+
+// =======================================================================
+//                            Declarations
+// =======================================================================
+
+#include "main.h"
+
+// =======================================================================
+//                            Cluster map
+// =======================================================================
+
+void MainFrame::InitMap()
+{
+    m_cMap = new wxStaticText(m_splitter,wxID_ANY,
+        wxT("The cluster map will be here"));
+}
+
+/** @} */
---
+++ trunk/src/wxgui/vollist.cpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  UltraDefrag - a powerful defragmentation tool for Windows NT.
+//  Copyright (c) 2007-2013 Dmitri Arkhangelski (dmitriar@gmail.com).
+//  Copyright (c) 2010-2013 Stefan Pendl (stefanpe@users.sourceforge.net).
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file vollist.cpp
+ * @brief List of volumes.
+ * @addtogroup VolList
+ * @{
+ */
+
+// Ideas by Stefan Pendl <stefanpe@users.sourceforge.net>
+// and Dmitri Arkhangelski <dmitriar@gmail.com>.
+
+// =======================================================================
+//                            Declarations
+// =======================================================================
+
+#include "main.h"
+
+// =======================================================================
+//                           List of volumes
+// =======================================================================
+
+void MainFrame::InitVolList()
+{
+    m_vList = new wxListView(m_splitter,wxID_ANY);
+}
+
+/** @} */
