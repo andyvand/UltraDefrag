832df38bd048f02ac2b3ad173344a234a9afe0f2
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-27 21:47:16 +0000

    wxgui is the working folder for the translation else the locale folder is created in the wrong folder

--- src/update-translations.cmd
+++ src/update-translations.cmd
@@ -39,7 +39,6 @@ pushd "%~dp0\wxgui"
 xgettext -C -j --copyright-holder="UltraDefrag Development Team" --msgid-bugs-address="http://sourceforge.net/p/ultradefrag/bugs/" -k_ -kwxPLURAL:1,2 -kwxTRANSLATE -kUD_UpdateMenuItemLabel:2 -o locale\UltraDefrag.pot *.cpp || goto fail
 copy /v /y locale\UltraDefrag.pot "%~dp0\tools\transifex\translations\ultradefrag.main\en_US.po"
 echo.
-popd
 
 :: download all translations from transifex
 pushd "%~dp0\tools\transifex"
