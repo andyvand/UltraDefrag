020436e1a492c3651b2c51f7ed3e1715e702cb0d
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-29 18:02:25 +0000

    cleanup

--- trunk/src/update-translations.cmd
+++ trunk/src/update-translations.cmd
@@ -36,6 +36,7 @@ set PATH=%GNUWIN32_DIR%;%PATH%
 pushd "%~dp0\wxgui"
 
 :: extract translations
+copy /v /y locale\UltraDefrag.header locale\UltraDefrag.pot
 xgettext -C -j --copyright-holder="UltraDefrag Development Team" --msgid-bugs-address="http://sourceforge.net/p/ultradefrag/bugs/" -k_ -kwxPLURAL:1,2 -kwxTRANSLATE -kUD_UpdateMenuItemLabel:2 -o locale\UltraDefrag.pot *.cpp || goto fail
 copy /v /y locale\UltraDefrag.pot "%~dp0\tools\transifex\translations\ultradefrag.main\en_US.po"
 echo.
