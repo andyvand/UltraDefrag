3bd86fa8459381fd937516993e8adb83f6bbfbd6
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-01-29 17:09:00 +0000

    Indonesian languages consolidated, Bahasa Indonesia will be official Indonesian translation

--- trunk/src/wxgui/i18n.cpp
+++ trunk/src/wxgui/i18n.cpp
@@ -71,12 +71,11 @@ void MainFrame::InitLocale()
 
     // add translations missing from wxWidgets
     wxLanguageInfo info;
-    UD_LNG(wxUD_LANGUAGE_BOSNIAN,           "bs"          , LANG_BOSNIAN   , SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_LATIN, wxLayout_LeftToRight, "Bosnian")
-    UD_LNG(wxUD_LANGUAGE_ILOKO,             "ilo"         , 0              , 0              , wxLayout_LeftToRight, "Iloko")
-    UD_LNG(wxUD_LANGUAGE_INDONESIAN_BAHASA, "id_ID@bahasa", LANG_INDONESIAN, SUBLANG_DEFAULT, wxLayout_LeftToRight, "Indonesian (Bahasa Indonesia)")
-    UD_LNG(wxUD_LANGUAGE_KAPAMPANGAN,       "pa_PH"       , 0              , 0              , wxLayout_LeftToRight, "Kapampangan")
-    UD_LNG(wxUD_LANGUAGE_NORWEGIAN,         "no"          , LANG_NORWEGIAN , SUBLANG_DEFAULT, wxLayout_LeftToRight, "Norwegian")
-    UD_LNG(wxUD_LANGUAGE_WARAY_WARAY,       "wa_PH"       , 0              , 0              , wxLayout_LeftToRight, "Waray-Waray")
+    UD_LNG(wxUD_LANGUAGE_BOSNIAN,     "bs" , LANG_BOSNIAN  , SUBLANG_BOSNIAN_BOSNIA_HERZEGOVINA_LATIN, wxLayout_LeftToRight, "Bosnian")
+    UD_LNG(wxUD_LANGUAGE_ILOKO,       "ilo", 0             , 0              , wxLayout_LeftToRight, "Iloko")
+    UD_LNG(wxUD_LANGUAGE_KAPAMPANGAN, "pam", 0             , 0              , wxLayout_LeftToRight, "Kapampangan")
+    UD_LNG(wxUD_LANGUAGE_NORWEGIAN,   "no" , LANG_NORWEGIAN, SUBLANG_DEFAULT, wxLayout_LeftToRight, "Norwegian")
+    UD_LNG(wxUD_LANGUAGE_WARAY_WARAY, "war", 0             , 0              , wxLayout_LeftToRight, "Waray-Waray")
 
     // get initial language selection
     int id = wxLANGUAGE_ENGLISH_US;
