6030c85ed9956eb7a9afb3dd36b3b892cbc3671b
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2014-10-19 01:27:30 +0000

    [RELEASE] 6.0.3 & 7.0.0 beta2

--- src/setvars.cmd
+++ src/setvars.cmd
@@ -8,7 +8,7 @@ set VERSION_REVISION=0
 
 :: alpha1, beta2, rc3, etc.
 :: unset for the final releases
-set RELEASE_STAGE=
+set RELEASE_STAGE=beta2
 
 :: paths to development tools
 set WINSDKBASE=C:\Program Files\Microsoft SDKs\Windows\v6.1
