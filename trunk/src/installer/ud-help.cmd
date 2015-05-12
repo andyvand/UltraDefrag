fdaac18050e400273487c330843af49a556d510b
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2015-05-12 06:07:02 +0000

    cleanup

--- trunk/src/installer/ud-help.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo.
-echo boot-config - configure boot time defragmenter
-echo boot-off    - disable boot time defragmenter
-echo boot-on     - enable boot time defragmenter
-echo udefrag     - perform a defragmentation job
