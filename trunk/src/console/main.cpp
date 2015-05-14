c4fe2aeb1d24b284c69636ae959e97286f5849df
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2015-05-14 19:25:54 +0000

    copyright years updated

--- trunk/src/console/main.cpp
+++ trunk/src/console/main.cpp
@@ -607,7 +607,7 @@ int __cdecl main(int argc, char **argv)
         return 0;
     }
 
-    printf(VERSIONINTITLE ", Copyright (c) UltraDefrag Development Team, 2007-2013.\n"
+    printf(VERSIONINTITLE ", Copyright (c) UltraDefrag Development Team, 2007-2015.\n"
         "UltraDefrag comes with ABSOLUTELY NO WARRANTY. This is free software, \n"
         "and you are welcome to redistribute it under certain conditions.\n\n"
     );
